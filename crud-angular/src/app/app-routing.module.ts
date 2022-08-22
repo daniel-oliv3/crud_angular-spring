1524f5ea1a800a3070c45b76209a320f306bfb10
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-22 12:10:23 -0300

    Update

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+
 const routes: Routes = [
-  {}
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
 ];
 
 @NgModule({
